25682f4525d4ccad76c1ffc3400adafcac767ac0
Antoine Cheminat <ac.dev@hotmail.com>
2022-07-31 09:55:58 -0400

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+  <title>Frontend Mentor | Todo app</title>
+</svelte:head>
+
+<h1>Todo App</h1>
---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/app.html", "./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    colors: {
+      blue: "hsl(220, 98%, 61%)",
+      cyan: "hsl(192, 100%, 67%)",
+      purple: "hsl(280, 87%, 65%)",
+      light: {
+        100: "hsl(0, 0%, 98%)",
+        200: "hsl(236, 33%, 92%)",
+        300: "hsl(233, 11%, 84%)",
+        400: "hsl(236, 9%, 61%)",
+        500: "hsl(235, 19%, 35%)",
+      },
+      dark: {
+        100: "hsl(236, 33%, 92%)",
+        200: "hsl(234, 39%, 85%)",
+        300: "hsl(234, 11%, 52%)",
+        400: "hsl(233, 14%, 35%)",
+        500: "hsl(237, 14%, 26%)",
+        600: "hsl(235, 24%, 19%)",
+        700: "hsl(235, 21%, 11%)",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
