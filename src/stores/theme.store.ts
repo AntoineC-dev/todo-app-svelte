a15a09e933c2644a4a562c702b99d9b1ccd54489
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-02 01:46:17 -0400

    Test persist theme data

--- src/stores/theme.store.ts
+++ src/stores/theme.store.ts
@@ -5,8 +5,19 @@ import { getPersistedData, updatePersistedData } from "../helpers/persistStore.h
 // export const theme = writable<Theme>(getPersistedData("theme") ?? "light");
 export const theme = writable<Theme>("light");
 
+// export const getThemeFromStorage = async () => {
+//   if (!localStorage.getItem("theme")) return null;
+//   try {
+//     const data = await JSON.parse(localStorage.getItem("theme") as string);
+//     return data;
+//   } catch (error) {
+//     console.log("Local storage error");
+//     console.log(error);
+//   }
+// };
+
 // Sync store & localStorage
-// theme.subscribe((current) => updatePersistedData("theme", current));
+theme.subscribe((current) => updatePersistedData("theme", current));
 
 // Toggle theme
 export const toggleTheme = () => theme.update((curr) => (curr === "light" ? "dark" : "light"));
