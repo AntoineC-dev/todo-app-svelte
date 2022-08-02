c35abed36de4c15df9b53a62e7aff38e5c8aa0c3
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-02 02:13:28 -0400

    Test 000

--- src/stores/theme.store.ts
+++ src/stores/theme.store.ts
@@ -1,23 +1,18 @@
 import { writable } from "svelte/store";
+import { browser } from "$app/env";
 import type { Theme } from "../types";
-import { getPersistedData, updatePersistedData } from "../helpers/persistStore.helper";
 
-// export const theme = writable<Theme>(getPersistedData("theme") ?? "light");
-export const theme = writable<Theme>("light");
+const defaultTheme: Theme = "light";
+const initialValue = browser ? (window.localStorage.getItem("theme") as Theme) ?? defaultTheme : defaultTheme;
 
-// export const getThemeFromStorage = async () => {
-//   if (!localStorage.getItem("theme")) return null;
-//   try {
-//     const data = await JSON.parse(localStorage.getItem("theme") as string);
-//     return data;
-//   } catch (error) {
-//     console.log("Local storage error");
-//     console.log(error);
-//   }
-// };
+export const theme = writable<Theme>(initialValue);
 
 // Sync store & localStorage
-theme.subscribe((current) => updatePersistedData("theme", current));
+theme.subscribe((current) => {
+  if (browser) {
+    window.localStorage.setItem("theme", current);
+  }
+});
 
 // Toggle theme
 export const toggleTheme = () => theme.update((curr) => (curr === "light" ? "dark" : "light"));
