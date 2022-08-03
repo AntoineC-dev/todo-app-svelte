eb99d41f15157ed76cf9981d0cd12dce30b97cf5
Antoine Cheminat <ac.dev@hotmail.com>
2022-08-03 01:26:52 -0400

    Fix accessibility issues

--- src/components/ToggleCompleted.svelte
+++ src/components/ToggleCompleted.svelte
@@ -4,24 +4,23 @@
   import check from "../assets/icon-check.svg";
 
   export let todo: Todo | undefined = undefined;
-  $: gradient = !todo
-    ? ""
-    : todo.completed
-    ? "completed-gradient"
-    : "[@media(hover:hover)]:hover:hover-gradient-light dark:[@media(hover:hover)]:hover:hover-gradient-dark";
+  $: gradient =
+    todo && todo.completed
+      ? "completed-gradient"
+      : "[@media(hover:hover)]:hover:hover-gradient-light dark:[@media(hover:hover)]:hover:hover-gradient-dark";
 </script>
 
-<button
-  aria-label={todo ? `Set todo to ${todo.completed ? "uncompleted" : "completed"}` : ""}
-  role={todo ? "button" : "none"}
-  disabled={!todo}
-  on:click|stopPropagation={() => todo && toggleTodoCompleted(todo.id)}
-  class="border-2 border-light-200 dark:border-dark-500 w-clamp-toggle h-clamp-toggle flex justify-center items-center rounded-full shrink-0 {gradient}"
->
-  <img
-    class:dark:hidden={!todo || (todo && !todo.completed)}
-    src={check}
-    alt=""
-    class="w-clamp-x-check h-clamp-y-check"
+{#if todo !== undefined}
+  <button
+    aria-label={`Set todo to ${todo.completed ? "uncompleted" : "completed"}`}
+    type="button"
+    on:click|stopPropagation={() => todo && toggleTodoCompleted(todo.id)}
+    class="border-2 border-light-200 dark:border-dark-500 w-clamp-toggle h-clamp-toggle flex justify-center items-center rounded-full shrink-0 {gradient}"
+  >
+    <img class:dark:hidden={todo && !todo.completed} src={check} alt="" class="w-clamp-x-check h-clamp-y-check" />
+  </button>
+{:else}
+  <div
+    class="border-2 border-light-200 dark:border-dark-500 w-clamp-toggle h-clamp-toggle flex justify-center items-center rounded-full shrink-0"
   />
-</button>
+{/if}
